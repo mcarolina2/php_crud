75b9c3630fb79ef37effdff2ec9130536964be3e
Carol <mariacfraga8@gmail.com>
2023-09-04 22:06:07 -0300

    Update add_cadastro.php

--- add_cadastro.php
+++ add_cadastro.php
@@ -1,6 +1,6 @@
 <?php 
-$nome = $_POST['Nome'];
-$email = $_POST['Email'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
 
 $fp = fopen('dados.csv', 'a');
 fwrite($fp, $nome . ',' . $email . PHP_EOL);
